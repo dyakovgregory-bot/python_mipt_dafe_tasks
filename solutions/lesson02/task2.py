d39b6809f573c5998cb9f7e891d41f313cdb02dd
GregoryD <dyakova.victoria1973@gmail.com>
2025-10-03 19:56:53 +0300

    remade 3 tasks

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,11 +1,5 @@
 def get_doubled_factorial(num: int) -> int:
     factorial = 1
-<<<<<<< HEAD
     for i in range(num, 1, -2):
         factorial *= i
     return factorial
-=======
-    for i in range(num,1,-2):
-        factorial *= i
-    return factorial
->>>>>>> 570d5112c7b91279809cacc78e469fc98a5febbd
--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,11 +1,7 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
     while num > 1:
-<<<<<<< HEAD
         for divisor in range(2, num + 1):
-=======
-        for divisor in range(2, num+1):
->>>>>>> 570d5112c7b91279809cacc78e469fc98a5febbd
             if num % divisor == 0:
                 sum_of_divisors += divisor
                 while num % divisor == 0:
